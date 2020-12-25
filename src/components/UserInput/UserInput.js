8d845b68bca6083c0697149d9b2d8d38ad52a5d5
Balázs Márton <bmarton87@gmail.com>
2020-12-25 21:42:16 +0100

    some ugly styling

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const userInput = (props) => {
+    const userInputStyle = {
+        border: '1px solid green',
+        'border-radius': '5px'
+    }
     return (
         <div>
-        <input type="text" onChange={props.changeHandler} value={props.name}/>
+        <input type="text" onChange={props.changeHandler} value={props.name} style={userInputStyle}/>
         </div>
     );
 }
