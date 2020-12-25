e211639f6a97a6e0d394f48522697c91a2c41009
Balázs Márton <bmarton87@gmail.com>
2020-12-25 14:34:10 +0100

    refact app to component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React, { Component } from 'react';
 import './App.css';
 import UserInput from './components/UserInput/UserInput';
 import UserOutput from './components/UserOutput/UserOutput';
 
-function App() {
-  return (
-    <div>
-      <UserInput/>
-      <UserOutput name="Joe"/>
-      <UserOutput name="John"/>
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <UserInput/>
+        <UserOutput name="Joe"/>
+        <UserOutput name="John"/>
     </div>
-    
-  );
+    )
+  }
 }
 
 export default App;
